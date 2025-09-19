362941bad21d218f835f409e5d5fc5f5962119f5
jakobdubeau <jakobdubeau@gmail.com>
2025-09-19 12:38:46 -0400

    learning webcam stuff

---
+++ plant_identifier.py
@@ -0,0 +1,16 @@
+import cv2
+
+cap = cv2.VideoCapture(0) # cam object, opens webcam
+
+while True: # get cam frames forever
+    ret, frame = cap.read() # ret = boolean return, frame = image(pixels). So, while capture reads, it returns a true/false if works and the image
+    if not ret: # if cam fails exit loop
+        break
+
+    cv2.imshow =('Video', frame) # image show takes in window title and the image to show
+
+    if cv2.waitKey(1) & 0xFF == ord('q'): # wait 1ms for a key press, if press q, exit loop. & 0xFF is so it works better and ord('q') is code for q key
+        break
+
+cap.release() # close cam
+cv2.destroyAllWindows() # close cv2 windows
