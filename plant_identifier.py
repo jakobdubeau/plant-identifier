2e62e6504df747ead7cc5b0f12740a832d925010
jakobdubeau <jakobdubeau@gmail.com>
2025-10-08 21:27:53 -0400

    added bounding boxes and object detection

--- plant_identifier.py
+++ plant_identifier.py
@@ -1,5 +1,7 @@
 import cv2
+from ultralytics import YOLO
 
+model = YOLO('yolo8n.pt') # load a custom yolo model
 cap = cv2.VideoCapture(0) # cam object, opens webcam
 
 cv2.namedWindow('Plant Identifier', cv2.WINDOW_NORMAL) # resizable window
@@ -10,6 +12,14 @@ while True: # get cam frames forever
         break
 
     frame = cv2.flip(frame, 1) # flip image horizontally, 0 = vertical, 1 = horizontal, -1 = both
+
+    results = model(frame) # takes frame, sends to model, returns a list of object found
+    for result in results:
+        boxes = result.boxes # boxes = bounding boxes object (3 objects = 3 boxes)
+        if boxes is not None:
+            for box in boxes: # for each individual box (object)
+                x1, y1, x2, y2 = map(int, box.xyxy[0]) # get box coordinates, convert to int. box.xyxy[0] is one detected object with coords, [0] is first set of coords, map int converts coords to int
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2) # cv2 function to draw rectangle on frame, bounded by x1, y1 and x2, y2 (box coords for result object), rgb colour for green, rectangle thickness
 
     cv2.imshow('Plant Identifier', frame) # image show takes in window title and the image to show
 
