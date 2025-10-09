53acecc2462c47437c0378eebb41f9543ad587b2
jakobdubeau <jakobdubeau@gmail.com>
2025-10-08 21:39:29 -0400

    object classification

--- plant_identifier.py
+++ plant_identifier.py
@@ -19,6 +19,11 @@ while True: # get cam frames forever
         if boxes is not None:
             for box in boxes: # for each individual box (object)
                 x1, y1, x2, y2 = map(int, box.xyxy[0]) # get box coordinates, convert to int. box.xyxy[0] is one detected object with coords, [0] is first set of coords, map int converts coords to int
+                class_id = int(box.cls[0]) # box.cls (category), [0] is class id, int to convert to whole number. basically turns classification to int?
+                class_name = model.names[class_id] # convert numbers to names with id
+                confidence = float(box.conf[0]) # confindence score
+                label = f"{class_name}: {confidence:.2f}" # text label containing object name and confidence (2 decimal places)
+                cv2.putText(frame, label, (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 2) # draw on frame, label for text, others self explanatory
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2) # cv2 function to draw rectangle on frame, bounded by x1, y1 and x2, y2 (box coords for result object), rgb colour for green, rectangle thickness
 
     cv2.imshow('Plant Identifier', frame) # image show takes in window title and the image to show
